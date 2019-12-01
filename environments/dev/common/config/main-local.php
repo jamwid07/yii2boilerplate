84a66c8cf39d3ceb5574ecd8d941ae228d873876
Jamshid <jamwid07@mail.ru>
2019-12-01 09:03:34 +0500

    add environment variables

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,17 +3,14 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host='.getenv('DB_HOST').';dbname=' . getenv('DB_NAME'),
+            'username' => getenv('DB_NAME'),
+            'password' => getenv('DB_PASS'),
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
     ],
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,14 +3,21 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host='.getenv('DB_HOST').';dbname=' . getenv('DB_NAME'),
+            'username' => getenv('DB_NAME'),
+            'password' => getenv('DB_PASS'),
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => getenv('EMAIL_SMTP_HOST'),
+                'username' => getenv('EMAIL_USER'),
+                'password' => getenv('EMAIL_PASS'),
+                'port' => getenv('EMAIL_PORT'),
+            ],
         ],
     ],
 ];
